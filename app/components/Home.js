64f5d1a31c382acdcf28a7e8f95b2a28729c3557
Cristal Tay <bluecolorpencils@gmail.com>
2016-08-23 23:48:55 -0700

    need to clean up/delete a bunch of trial files. added the fb logout button to the user page. need to redirect it to the home page

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,7 +6,7 @@ import {
   View
 } from 'react-native';
 
-import Launch from './Launch';
+import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 
 import ScrollableTabView, { DefaultTabBar, } from 'react-native-scrollable-tab-view';
 import FacebookTabBar from './FacebookTabBar';
@@ -39,8 +39,9 @@ export default React.createClass({
 
 
       <ScrollView tabLabel="account-box">
-        {Launch}
-
+        <View style={styles.innercontainer}>
+          <FBLogin />
+        </View>
       </ScrollView>
 
 
@@ -172,123 +173,3 @@ const styles = StyleSheet.create({
 //         marginBottom: 5,
 //     },
 // });
-//
-// // import React from 'react';
-// // import {View, Text, StyleSheet, ToolbarAndroid} from "react-native";
-// // import Button from "react-native-button";
-// // import {Actions} from "react-native-router-flux";
-// // import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
-//
-// //
-// // var styles = StyleSheet.create({
-// //     container: {
-// //       // marginTop: 150,
-// //       flex: 1,
-// //       alignItems: 'center',
-// //       justifyContent: 'center',
-// //       backgroundColor: "#F5FCFF"
-// //     },
-// //     innercontainer: {
-// //         flex: 1,
-// //         justifyContent: "center",
-// //         alignItems: "center",
-// //         backgroundColor: "#F5FCFF"
-// //     },
-// //     welcome: {
-// //         fontSize: 20,
-// //         textAlign: "center",
-// //         margin: 10
-// //     },
-// //     instructions: {
-// //         textAlign: "center",
-// //         color: "#333333",
-// //         marginBottom: 5,
-// //     },
-// // });
-// //
-// // class Home extends React.Component {
-// //     render(){
-// //         var _this = this;
-// //         return (
-// //           <View style={styles.container}>
-// //           <ToolbarAndroid
-// //             style={styles.nav}
-// //             title="AwesomeApp"
-// //             actions={[{title: 'TOOLBAR', show: 'always'}]}
-// //             onActionSelected={this.onActionSelected} />
-// //
-// //             <View style={styles.innercontainer}>
-// //                 <Text>MAIN SCREEN</Text>
-// //             </View>
-// //
-// //             <FBLogin style={styles.loginbutton}
-// //               onLogout={function(){
-// //                 console.log("Logged out.");
-// //                 _this.setState({ user : null });
-// //               }}
-// //             />
-// //           </View>
-// //         );
-// //     }
-// // }
-// //
-// //
-// //
-// // var styles = StyleSheet.create({
-// //     nav: {
-// //       padding: 40,
-// //       flex: 1
-// //
-// //     }
-// //   })
-// //
-// module.exports = Home;
-//
-// import React from 'react';
-// import {PropTypes} from "react";
-// import {StyleSheet, Text, View} from "react-native";
-// import Button from 'react-native-button';
-// import { Actions } from 'react-native-router-flux';
-//
-// const contextTypes = {
-//   drawer: React.PropTypes.object,
-// };
-//
-// const propTypes = {
-//   name: PropTypes.string,
-//   sceneStyle: View.propTypes.style,
-//   title: PropTypes.string,
-// };
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//     borderWidth: 1,
-//     borderColor: 'red',
-//   },
-// });
-//
-// const TabView = (props, context) => {
-//   const drawer = context.drawer;
-//   return (
-//     <View style={[styles.container, props.sceneStyle ]}>
-//       <Text>Tab {props.title}</Text>
-//       {props.name === 'tab1_1' &&
-//         <Button onPress={Actions.tab1_2}>next screen for tab1_1</Button>
-//       }
-//       {props.name === 'tab2_1' &&
-//         <Button onPress={Actions.tab2_2}>next screen for tab2_1</Button>
-//       }
-//       <Button onPress={Actions.pop}>Back</Button>
-//
-//     </View>
-//   );
-// };
-//
-// TabView.contextTypes = contextTypes;
-// TabView.propTypes = propTypes;
-//
-// export default TabView;
